69e85e8daee185a668a7659940f3e5aed885c9c9
lee-ck <leeck@kaist.ac.kr>
2024-07-03 16:36:38 +0900

    kiyong

--- acados_heron/c_generated_code/heron_constraints/heron_constr_h_e_fun_jac_uxt_zt.c
+++ acados_heron/c_generated_code/heron_constraints/heron_constr_h_e_fun_jac_uxt_zt.c
@@ -36,6 +36,7 @@ extern "C" {
 #define casadi_s3 CASADI_PREFIX(s3)
 #define casadi_s4 CASADI_PREFIX(s4)
 #define casadi_s5 CASADI_PREFIX(s5)
+#define casadi_sq CASADI_PREFIX(sq)
 
 /* Symbol visibility in DLLs */
 #ifndef CASADI_SYMBOL_EXPORT
@@ -52,28 +53,95 @@ extern "C" {
   #endif
 #endif
 
+casadi_real casadi_sq(casadi_real x) { return x*x;}
+
 static const casadi_int casadi_s0[11] = {7, 1, 0, 7, 0, 1, 2, 3, 4, 5, 6};
 static const casadi_int casadi_s1[3] = {0, 0, 0};
-static const casadi_int casadi_s2[7] = {3, 1, 0, 3, 0, 1, 2};
-static const casadi_int casadi_s3[5] = {1, 1, 0, 1, 0};
-static const casadi_int casadi_s4[6] = {7, 1, 0, 2, 0, 1};
-static const casadi_int casadi_s5[3] = {1, 0, 0};
+static const casadi_int casadi_s2[19] = {15, 1, 0, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+static const casadi_int casadi_s3[9] = {5, 1, 0, 5, 0, 1, 2, 3, 4};
+static const casadi_int casadi_s4[18] = {7, 5, 0, 2, 4, 6, 8, 10, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+static const casadi_int casadi_s5[3] = {5, 0, 0};
 
-/* heron_constr_h_e_fun_jac_uxt_zt:(i0[7],i1[],i2[],i3[3])->(o0,o1[7x1,2nz],o2[1x0]) */
+/* heron_constr_h_e_fun_jac_uxt_zt:(i0[7],i1[],i2[],i3[15])->(o0[5],o1[7x5,10nz],o2[5x0]) */
 static int casadi_f0(const casadi_real** arg, casadi_real** res, casadi_int* iw, casadi_real* w, int mem) {
-  casadi_real a0, a1, a2, a3;
+  casadi_real a0, a1, a10, a11, a2, a3, a4, a5, a6, a7, a8, a9;
   a0=arg[0]? arg[0][0] : 0;
   a1=arg[3]? arg[3][0] : 0;
-  a0=(a0*a1);
-  a2=arg[0]? arg[0][1] : 0;
-  a3=arg[3]? arg[3][1] : 0;
-  a2=(a2*a3);
-  a0=(a0+a2);
-  a2=arg[3]? arg[3][2] : 0;
-  a0=(a0+a2);
-  if (res[0]!=0) res[0][0]=a0;
+  a1=(a0-a1);
+  a2=casadi_sq(a1);
+  a3=arg[0]? arg[0][1] : 0;
+  a4=arg[3]? arg[3][1] : 0;
+  a4=(a3-a4);
+  a5=casadi_sq(a4);
+  a2=(a2+a5);
+  a5=arg[3]? arg[3][2] : 0;
+  a5=casadi_sq(a5);
+  a2=(a2-a5);
+  if (res[0]!=0) res[0][0]=a2;
+  a2=arg[3]? arg[3][3] : 0;
+  a2=(a0-a2);
+  a5=casadi_sq(a2);
+  a6=arg[3]? arg[3][4] : 0;
+  a6=(a3-a6);
+  a7=casadi_sq(a6);
+  a5=(a5+a7);
+  a7=arg[3]? arg[3][5] : 0;
+  a7=casadi_sq(a7);
+  a5=(a5-a7);
+  if (res[0]!=0) res[0][1]=a5;
+  a5=arg[3]? arg[3][6] : 0;
+  a5=(a0-a5);
+  a7=casadi_sq(a5);
+  a8=arg[3]? arg[3][7] : 0;
+  a8=(a3-a8);
+  a9=casadi_sq(a8);
+  a7=(a7+a9);
+  a9=arg[3]? arg[3][8] : 0;
+  a9=casadi_sq(a9);
+  a7=(a7-a9);
+  if (res[0]!=0) res[0][2]=a7;
+  a7=arg[3]? arg[3][9] : 0;
+  a7=(a0-a7);
+  a9=casadi_sq(a7);
+  a10=arg[3]? arg[3][10] : 0;
+  a10=(a3-a10);
+  a11=casadi_sq(a10);
+  a9=(a9+a11);
+  a11=arg[3]? arg[3][11] : 0;
+  a11=casadi_sq(a11);
+  a9=(a9-a11);
+  if (res[0]!=0) res[0][3]=a9;
+  a9=arg[3]? arg[3][12] : 0;
+  a0=(a0-a9);
+  a9=casadi_sq(a0);
+  a11=arg[3]? arg[3][13] : 0;
+  a3=(a3-a11);
+  a11=casadi_sq(a3);
+  a9=(a9+a11);
+  a11=arg[3]? arg[3][14] : 0;
+  a11=casadi_sq(a11);
+  a9=(a9-a11);
+  if (res[0]!=0) res[0][4]=a9;
+  a1=(a1+a1);
   if (res[1]!=0) res[1][0]=a1;
-  if (res[1]!=0) res[1][1]=a3;
+  a4=(a4+a4);
+  if (res[1]!=0) res[1][1]=a4;
+  a2=(a2+a2);
+  if (res[1]!=0) res[1][2]=a2;
+  a6=(a6+a6);
+  if (res[1]!=0) res[1][3]=a6;
+  a5=(a5+a5);
+  if (res[1]!=0) res[1][4]=a5;
+  a8=(a8+a8);
+  if (res[1]!=0) res[1][5]=a8;
+  a7=(a7+a7);
+  if (res[1]!=0) res[1][6]=a7;
+  a10=(a10+a10);
+  if (res[1]!=0) res[1][7]=a10;
+  a0=(a0+a0);
+  if (res[1]!=0) res[1][8]=a0;
+  a3=(a3+a3);
+  if (res[1]!=0) res[1][9]=a3;
   return 0;
 }
 
--- acados_heron/c_generated_code/heron_model/heron_impl_dae_fun.c
+++ acados_heron/c_generated_code/heron_model/heron_impl_dae_fun.c
@@ -56,9 +56,9 @@ casadi_real casadi_sq(casadi_real x) { return x*x;}
 static const casadi_int casadi_s0[11] = {7, 1, 0, 7, 0, 1, 2, 3, 4, 5, 6};
 static const casadi_int casadi_s1[6] = {2, 1, 0, 2, 0, 1};
 static const casadi_int casadi_s2[3] = {0, 0, 0};
-static const casadi_int casadi_s3[7] = {3, 1, 0, 3, 0, 1, 2};
+static const casadi_int casadi_s3[19] = {15, 1, 0, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-/* heron_impl_dae_fun:(i0[7],i1[7],i2[2],i3[],i4[],i5[3])->(o0[7]) */
+/* heron_impl_dae_fun:(i0[7],i1[7],i2[2],i3[],i4[],i5[15])->(o0[7]) */
 static int casadi_f0(const casadi_real** arg, casadi_real** res, casadi_int* iw, casadi_real* w, int mem) {
   casadi_real a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
   a0=arg[1]? arg[1][0] : 0;
