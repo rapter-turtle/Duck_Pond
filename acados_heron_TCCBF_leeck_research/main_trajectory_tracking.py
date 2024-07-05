96a21cb3f2c7b38b9627e37c81c9c3e1f1450f21
lee-ck <leeck@kaist.ac.kr>
2024-07-05 15:56:47 +0900

    cbf lck update

---
+++ acados_heron_TCCBF_leeck_research/main_trajectory_tracking.py
@@ -0,0 +1,133 @@
+from plot_asv import animateASV, plot_inputs
+from gen_ref import *
+from acados_setting import *
+
+def main():
+
+    Fmax = 45
+    N_horizon = 20
+    dt = 0.2
+    Tf = int(dt*N_horizon)
+    Nsim = 1000
+
+    con_dt = dt
+    ref_dt = 0.01
+    ref_iter = int(con_dt/ref_dt)
+
+    reference = generate_figure_eight_trajectory((Nsim+N_horizon*2)*con_dt, ref_dt)
+    # reference = generate_figure_eight_trajectory_con(100, ref_dt)
+    reference = reference[::ref_iter,:]
+
+    x0 = np.array([0.0, 2.0, 0.0 , 0.5, 0.0, 0, 0])
+    ocp_solver, integrator = setup_trajectory_tracking(x0, Fmax, N_horizon, Tf)
+
+    nx = ocp_solver.acados_ocp.dims.nx
+    nu = ocp_solver.acados_ocp.dims.nu
+
+    simX = np.zeros((Nsim+1, nx))
+    simU = np.zeros((Nsim+1, nu))
+    simX[0,:] = x0
+    yref_list = []
+    obs_list = []
+    mpc_pred_list = []
+
+    t_preparation = np.zeros((Nsim))
+    t_feedback = np.zeros((Nsim))
+
+    # do some initial iterations to start with a good initial guess
+    num_iter_initial = 5
+    for _ in range(num_iter_initial):
+        ocp_solver.solve_for_x0(x0_bar = x0)
+
+    # closed loop
+    for i in range(Nsim):
+        print(i)
+
+        for j in range(N_horizon):
+            yref = np.hstack((reference[i+j,:],0,0,0,0))
+            ocp_solver.cost_set(j, "yref", yref)
+        
+        yref = np.hstack((reference[i:i + N_horizon+1, :], np.zeros((N_horizon+1, 2))))
+        yref_list.append(yref)
+
+        yref_N = np.hstack((reference[i+N_horizon,:],0,0))
+        ocp_solver.cost_set(N_horizon, "yref", yref_N)
+
+        rad = 1
+        obs_pos = np.array([0.0, 0.1, 0.5, 
+                            6.0, -1.0*np.sin(i/10), 0.6, 
+                            3.0, 1.0, 0.4, 
+                            -3.0, 1.0, 0.4,
+                            -6.0, 1.0*np.sin(i/10), 0.5])
+        obs_list.append(obs_pos)
+
+        for j in range(N_horizon):
+            obs_pos = np.array([0.0, 0.1, 0.5, 
+                                6.0, -1.0*np.sin((i+j*con_dt)/10), 0.6, 
+                                3.0, 1.0, 0.4, 
+                                -3.0, 1.0, 0.4,
+                                -6.0, 1.0*np.sin((i+j*con_dt)/10), 0.5])
+            ocp_solver.set(j, "p", obs_pos)
+        obs_pos = np.array([0.0, 0.1, 0.5, 
+                            6.0, -1.0*np.sin((i+j*con_dt)/10), 0.6, 
+                            3.0, 1.0, 0.4, 
+                            -3.0, 1.0, 0.4,
+                            -6.0, 1.0*np.sin((i+N_horizon*con_dt)/10), 0.5])
+        ocp_solver.set(N_horizon, "p", obs_pos)
+
+
+        # preparation phase
+        ocp_solver.options_set('rti_phase', 1)
+        status = ocp_solver.solve()
+        t_preparation[i] = ocp_solver.get_stats('time_tot')
+
+        # set initial state
+        ocp_solver.set(0, "lbx", simX[i, :])
+        ocp_solver.set(0, "ubx", simX[i, :])
+
+        # feedback phase
+        ocp_solver.options_set('rti_phase', 2)
+        status = ocp_solver.solve()
+        t_feedback[i] = ocp_solver.get_stats('time_tot')
+
+        simU[i, :] = ocp_solver.get(0, "u")
+        
+        print(t_preparation[i] + t_feedback[i])
+
+        mpc_pred = []
+        for j in range(N_horizon+1):
+            mpc_pred.append(ocp_solver.get(j, "x")[0:2]) 
+        mpc_pred_array = np.vstack(mpc_pred)
+        mpc_pred_list.append(mpc_pred_array)
+
+
+        # simulate system
+        simX[i+1, :] = integrator.simulate(x=simX[i, :], u=simU[i,:])
+
+        # print(simX[i, 3])
+        # print(simU[i, :])
+    simU[i+1, :] = simU[i, :]
+    yref_list.append(yref)
+    mpc_pred_list.append(mpc_pred_array)
+
+    # evaluate timings
+    # scale to milliseconds
+    t_preparation *= 1000
+    t_feedback *= 1000
+    print(f'Computation time in preparation phase in ms: \
+            min {np.min(t_preparation):.3f} median {np.median(t_preparation):.3f} max {np.max(t_preparation):.3f}')
+    print(f'Computation time in feedback phase in ms:    \
+            min {np.min(t_feedback):.3f} median {np.median(t_feedback):.3f} max {np.max(t_feedback):.3f}')
+    yref_array = np.array(yref_list)
+    obs_array = np.array(obs_list)
+
+    ocp_solver = None
+    plot_iter = 3
+    animateASV(simX[::plot_iter,:], simU[::plot_iter,:], reference, yref_array[::plot_iter],mpc_pred_list[::plot_iter], obs_array)
+
+    t = np.arange(0, con_dt*Nsim, con_dt)
+    plot_inputs(t, reference, simX, simU, Fmax)
+
+
+if __name__ == '__main__':
+    main()
---
+++ acados_heron_TCCBF_leeck_research/plot_asv.py
@@ -0,0 +1,148 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+
+def animateASV(states, inputs, ref, yref, mpc_result, obs_pos):
+    # Define the geometry of the twin-hull ASV
+    hullLength = 0.7  # Length of the hull
+    hullWidth = 0.2   # Width of each hull
+    separation = 0.4  # Distance between the two hulls
+    bodyLength = hullLength  # Length of the body connecting the hulls
+    bodyWidth = 0.25  # Width of the body connecting the hulls
+
+    fig, ax = plt.subplots()
+
+    def update(frame):
+        position = states[frame,0:2]
+        heading = states[frame,2]
+        print(inputs[frame,0:2])
+        print()
+        force_left = states[frame,5]/100
+        force_right = states[frame,6]/100
+        # Clear the previous plot
+        ax.clear()
+        
+        # Define the vertices of the two hulls
+        hull1 = np.array([[-hullLength/2, hullLength/2, hullLength/2, -hullLength/2, -hullLength/2, -hullLength/2],
+                          [hullWidth/2, hullWidth/2, -hullWidth/2, -hullWidth/2, 0, hullWidth/2]])
+
+        hull2 = np.array([[-hullLength/2, hullLength/2, hullLength/2, -hullLength/2, -hullLength/2, -hullLength/2],
+                          [hullWidth/2, hullWidth/2, -hullWidth/2, -hullWidth/2, 0, hullWidth/2]])
+
+        # Define the vertices of the body connecting the hulls
+        body = np.array([[-bodyWidth/2, bodyWidth/2, bodyWidth/2, -bodyWidth/2, -bodyWidth/2],
+                         [separation/2, separation/2, -separation/2, -separation/2, separation/2]])
+
+        # Combine hulls into a single structure
+        hull2[1, :] = hull2[1, :] - separation/2
+        hull1[1, :] = hull1[1, :] + separation/2
+
+        # Rotation matrix for the heading
+        R = np.array([[np.cos(heading), -np.sin(heading)],
+                      [np.sin(heading), np.cos(heading)]])
+
+        # Rotate the hulls and body
+        hull1 = R @ hull1
+        hull2 = R @ hull2
+        body = R @ body
+
+        # Translate the hulls and body to the specified position
+        hull1 = hull1 + np.array(position).reshape(2, 1)
+        hull2 = hull2 + np.array(position).reshape(2, 1)
+        body = body + np.array(position).reshape(2, 1)
+
+        # Calculate the direction vector for the heading
+        arrow_length = 0.5
+        direction = np.array([np.cos(heading), np.sin(heading)]) * arrow_length
+
+
+        # Calculate the positions for the force vectors
+        force_left_position = hull1[:, 4]
+        force_right_position = hull2[:, 4]
+
+        # Plot the force vectors
+        if force_left > 0:
+            ax.arrow(force_left_position[0], force_left_position[1], force_left*np.cos(heading), force_left*np.sin(heading), head_width=0.1, head_length=0.1, fc='b', ec='b')
+        else:
+            ax.arrow(force_left_position[0], force_left_position[1], force_left*np.cos(heading), force_left*np.sin(heading), head_width=0.1, head_length=0.1, fc='r', ec='r')
+        
+        if force_right > 0:
+            ax.arrow(force_right_position[0], force_right_position[1], force_right*np.cos(heading), force_right*np.sin(heading), head_width=0.1, head_length=0.1, fc='b', ec='b')
+        else:
+            ax.arrow(force_right_position[0], force_right_position[1], force_right*np.cos(heading), force_right*np.sin(heading), head_width=0.1, head_length=0.1, fc='r', ec='r')
+
+        # Plot the ASV
+        ax.fill(hull1[0, :], hull1[1, :], 'b', alpha=0.3)
+        ax.fill(hull2[0, :], hull2[1, :], 'b', alpha=0.3)
+        ax.fill(body[0, :], body[1, :], 'b', alpha=0.3)
+        ax.plot(position[0], position[1], 'go')  # Mark the position with a red dot
+        ax.arrow(position[0], position[1], direction[0], direction[1], head_width=0.1, head_length=0.1, fc='k', ec='k')
+        ax.axis('equal')
+
+        ax.plot(ref[:,0], ref[:,1], 'k--')
+        ax.plot(yref[frame,:,0], yref[frame,:,1], 'mo')
+        ax.plot(mpc_result[frame][:,0], mpc_result[frame][:,1], 'c.')
+
+
+        theta = np.linspace( 0 , 2 * np.pi , 150 )
+        
+        for i in range(5):
+            radius = obs_pos[frame][3*i+2]        
+            a = obs_pos[frame][3*i+0] + radius * np.cos( theta )
+            b = obs_pos[frame][3*i+1] + radius * np.sin( theta )    
+            ax.plot(a, b)
+
+        ax.set_xlim(-8, 8)  # Adjust these limits as needed
+        ax.set_ylim(-6, 6)  # Adjust these limits as needed
+        # ax.autoscale(enable=True, axis='x', tight=True)
+        # ax.autoscale(enable=True, axis='y', tight=True)
+
+    anim = FuncAnimation(fig, update, frames=len(states), repeat=False)
+    plt.show()
+
+
+def plot_inputs(t, reference, states, inputs, Fmax):
+    FS = 18
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+    # Plot the figure-eight trajectory
+    axs[0, 0].plot(t, states[0:-1,3], 'k', linewidth=2) 
+    axs[0, 0].plot(t, reference[0:len(t),3], 'b--', alpha=0.7)
+    axs[0, 0].set_xlabel("Time", fontsize=FS)
+    axs[0, 0].set_ylabel("Speed [m/s]", fontsize=FS)
+    axs[0, 0].grid(True)
+    axs[0, 0].autoscale(enable=True, axis='x', tight=True)
+    axs[0, 0].autoscale(enable=True, axis='y', tight=True)
+
+    # Plot the headings
+    axs[0, 1].plot(t, states[0:-1,4], 'k', linewidth=2) 
+    axs[0, 1].plot(t, reference[0:len(t),4], 'b--', alpha=0.7)
+    axs[0, 1].set_xlabel("Time", fontsize=FS)
+    axs[0, 1].set_ylabel("Rot. Speed [rad/s]", fontsize=FS)
+    axs[0, 1].grid(True)
+    axs[0, 1].autoscale(enable=True, axis='x', tight=True)
+    axs[0, 1].autoscale(enable=True, axis='y', tight=True)
+
+    # Plot the figure-eight trajectory
+    axs[1, 0].plot(t, states[0:-1,5], 'k')
+    axs[1, 0].plot(t, states[0:-1,5]*0 + Fmax, 'r--')
+    axs[1, 0].plot(t, states[0:-1,5]*0 - Fmax, 'r--')
+    axs[1, 0].set_xlabel("Time", fontsize=FS)
+    axs[1, 0].set_ylabel("Left Thrust", fontsize=FS)
+    axs[1, 0].grid(True)
+    axs[1, 0].autoscale(enable=True, axis='x', tight=True)
+    axs[1, 0].set_ylim(-Fmax-1, Fmax+1)
+
+    # Plot the headings
+    axs[1, 1].plot(t, states[0:-1,6], 'k')
+    axs[1, 1].plot(t, states[0:-1,6]*0 + Fmax, 'r--')
+    axs[1, 1].plot(t, states[0:-1,6]*0 - Fmax, 'r--')
+    axs[1, 1].set_xlabel("Time", fontsize=FS)
+    axs[1, 1].set_ylabel("Right Thrust", fontsize=FS)
+    axs[1, 1].grid(True)
+    axs[1, 1].autoscale(enable=True, axis='x', tight=True)
+    axs[1, 1].set_ylim(-Fmax-1, Fmax+1)
+
+    plt.tight_layout()
+    plt.show()
